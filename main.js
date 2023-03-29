ca18e863939e0922370c49608db0d9cab6fe7738
andreinikolabai <andreiplus2000@gmail.com>
2023-03-29 15:55:09 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const changeImg = ()=> {
+    const rand = Math.ceil(Math.random() * 9)
+    document.querySelector('img').src = 'images/' + rand + '.jpg'
+}
+changeImg();
+
+document.querySelector('button').addEventListener('click', changeImg);
